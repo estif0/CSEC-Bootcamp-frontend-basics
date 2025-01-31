a59af3f575756e8ca420158429d9038de69ea146
Estifanose Sahilu <estifanoswork@gmail.com>
2025-01-31 22:04:22 +0300

    fix - Remove redundant div elements

--- csec-dev-react-js-day-one/src/main.tsx
+++ csec-dev-react-js-day-one/src/main.tsx
@@ -4,10 +4,8 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div className="flex items-center justify-center min-h-screen bg-slate-800">
-      <div className="flex flex-col my-6 space-y-6 md:space-y-0 md:space-x-6 md:flex-row md:my-0">
+
         <App />
-      </div>
-    </div>
+
   </StrictMode>
 );
