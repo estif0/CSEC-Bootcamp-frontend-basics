603d743b9a8f127a85a07f1e339ad309f3574408
Estifanose Sahilu <estifanoswork@gmail.com>
2025-01-31 15:51:20 +0300

    feat - Create a separate component that can handle props with dummy data

--- csec-dev-react-js-day-one/src/App.tsx
+++ csec-dev-react-js-day-one/src/App.tsx
@@ -1,78 +1,42 @@
 import "./App.css";
-
+import { PricingCard } from "./components/PricingCard";
+import { PricingCardProps } from "./components/PricingCard";
 function App() {
+  const pricingDetails: PricingCardProps[] = [
+    {
+      planName: "Basic",
+      pricePerMonth: 10,
+      storage: "10GB",
+      features: ["Unlimited Bandwidth", "10 Email Accounts", "Basic Support"],
+    },
+    {
+      planName: "Pro",
+      pricePerMonth: 20,
+      storage: "20GB",
+      features: [
+        "Unlimited Bandwidth",
+        "50 Email Accounts",
+        "Priority Support",
+      ],
+    },
+    {
+      planName: "Enterprise",
+      pricePerMonth: 30,
+      storage: "30GB",
+      features: [
+        "Unlimited Bandwidth",
+        "Unlimited Email Accounts",
+        "Dedicated Support",
+      ],
+    },
+  ];
   return (
     <>
-      <div className="bg-slate-700 rounded-xl text-white">
-        <div className="p-8 mx-3 mt-3 rounded-t-xl bg-slate-800">
-          <div className="text-center uppercase">Basic</div>
-          <h2 className="mt-10 font-serif text-5xl text-center">100GB</h2>
-          <h3 className="mt-2 text-center">$1.99/Month</h3>
-          <div className="flex justify-center">
-            <a
-              href="#"
-              className="inline-block px-10 py-3 my-6 text-center border border-violet-600 rounded-lg duration-200 hover:bg-violet-800 hover:border-violet-800"
-            >
-              Purchase
-            </a>
-          </div>
-        </div>
-
-        <div className="border-t border-slate-700"></div>
-
-        <div className="p-8 mx-3 mb-3 rounded-b-xl bg-slate-800">
-          <div className="flex flex-col space-y-2">
-            <div className="flex justify-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5"
-                viewBox="0 0 24 24"
-                stroke-width="2"
-                stroke="currentColor"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M5 12l5 5l10 -10" />
-              </svg>
-              <span className="text-sm ml-1">100 GB of storage</span>
-            </div>
-
-            <div className="flex justify-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5"
-                viewBox="0 0 24 24"
-                stroke-width="2"
-                stroke="currentColor"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M5 12l5 5l10 -10" />
-              </svg>
-              <span className="text-sm ml-1">Option to add members</span>
-            </div>
-
-            <div className="flex justify-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5"
-                viewBox="0 0 24 24"
-                stroke-width="2"
-                stroke="currentColor"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M5 12l5 5l10 -10" />
-              </svg>
-              <span className="text-sm ml-1">Extra member benefits</span>
-            </div>
-          </div>
+      <div className="flex items-center justify-center min-h-screen bg-slate-800">
+        <div className="flex flex-col my-6 space-y-6 md:space-y-0 md:space-x-6 md:flex-row md:my-0">
+          {pricingDetails.map((pricingDetail, index) => (
+            <PricingCard key={index} {...pricingDetail} />
+          ))}
         </div>
       </div>
     </>
