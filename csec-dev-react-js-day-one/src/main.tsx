f33ef871945062e5c1e5861cd6788215c8b772c7
Estifanose Sahilu <estifanoswork@gmail.com>
2025-01-31 15:07:55 +0300

    feat - Add basic pricing card

--- csec-dev-react-js-day-one/src/main.tsx
+++ csec-dev-react-js-day-one/src/main.tsx
@@ -4,6 +4,10 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <div className="flex items-center justify-center min-h-screen bg-slate-800">
+      <div className="flex flex-col my-6 space-y-6 md:space-y-0 md:space-x-6 md:flex-row md:my-0">
+        <App />
+      </div>
+    </div>
   </StrictMode>
 );
